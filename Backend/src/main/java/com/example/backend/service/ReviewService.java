d90be7e82d81381bb6602bf3509ad68348c7203b
Zuzansas <zuzanna.niciak@gmail.com>
2026-08-10 17:39:30 +0200

    .

--- Backend/src/main/java/com/example/backend/service/ReviewService.java
+++ Backend/src/main/java/com/example/backend/service/ReviewService.java
@@ -9,12 +9,9 @@ import com.example.backend.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
-
-import javax.crypto.BadPaddingException;
 
 @Service
 @RequiredArgsConstructor
